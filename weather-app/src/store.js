72de06caef717f80adcf5f2189acd9f2645a924b
RN848 <145915641+RN848@users.noreply.github.com>
2025-01-13 13:38:29 +0300

    Delete weather-app directory

--- weather-app/src/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import weatherApiSliceReducer from "./weatherApiSlice";
-
-export default configureStore({
-	reducer: { weather: weatherApiSliceReducer },
-});
